2a1810d167fd047a05c1a715f55172549452806b
Trinidad Leonardo <ttleonardo@hotmail.com>
2018-11-15 09:08:48 -0600

    first commit

--- src/helper/Persona.java
+++ src/helper/Persona.java
@@ -51,4 +51,5 @@ public abstract class Persona {
     }
     
     abstract void informacionPersonal();
+    
 }
---
+++ src/tareadegrupo10/TareadeGrupo10.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareadegrupo10;
+
+/**
+ *
+ * @author ttleo
+ */
+public class TareadeGrupo10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
